b6169f9500abed532cb7ed28b657c3a427bc2457
ColleenFernandez <colleensproit@gmail.com>
2020-10-09 22:43:46 +0300

    iPhone update

---
+++ iPhone/RentLA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RentLA
+//
+//  Created by Admin on 10/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
